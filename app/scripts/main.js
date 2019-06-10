d410f40d9dbecdea4ee4adf9c8d9407011d66f67
Thijs Van der Schaeghe <thijs@catlab.be>
2019-06-10 17:04:56 +0200

    Prepare easelbone for umd.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,14 +1,14 @@
 require.config ({
 	paths: {
-		EaselJS: "vendor/EaselJS/lib/easeljs-NEXT.combined",
-		PreloadJS: "vendor/PreloadJS/lib/preloadjs-NEXT.combined",
-		SoundJS: "vendor/SoundJS/lib/soundjs-NEXT.combined",
-		TweenJS: "vendor/TweenJS/lib/tweenjs-NEXT.combined",
-		jquery: "vendor/jquery/dist/jquery",
-		requirejs: "vendor/requirejs/require",
-		underscore: "vendor/underscore/underscore",
-		backbone: "vendor/backbone/backbone",
-		easelhacks: "vendor/easelhacks/dist/scripts/easelhacks"
+		EaselJS: "../../node_modules/EaselJS/lib/easeljs",
+		PreloadJS: "../../node_modules/PreloadJS/lib/preloadjs",
+		SoundJS: "../../node_modules/SoundJS/lib/soundjs",
+		TweenJS: "../../node_modules/TweenJS/lib/tweenjs",
+		jquery: "../../node_modules/jquery/dist/jquery",
+		requirejs: "../../node_modules/requirejs/require",
+		underscore: "../../node_modules/underscore/underscore",
+		backbone: "../../node_modules/backbone/backbone",
+		easelhacks: "../../node_modules/easelhacks/dist/scripts/easelhacks"
 	},
 	shim: {
 		EaselJS: {
