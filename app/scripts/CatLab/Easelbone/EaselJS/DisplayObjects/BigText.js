486de3c0eec8fdccba3ab0644d8474ee9113dabd
Thijs Van der Schaeghe <thijs@catlab.be>
2023-06-06 02:10:05 +0200

    Fix buttons and bigtexts not updating on parent bound changes (and not sure what is causing it)

--- app/scripts/CatLab/Easelbone/EaselJS/DisplayObjects/BigText.js
+++ app/scripts/CatLab/Easelbone/EaselJS/DisplayObjects/BigText.js
@@ -163,14 +163,18 @@ define(
         };
 
         p.getAvailableSpace = function () {
+
             if (this.limits !== null) {
                 return this.limits;
-            } else if (this.getBounds()) {
-                width = this.getBounds().width;
-                height = this.getBounds().height;
+            } else if (this._bounds) {
+                width = this._bounds.width;
+                height = this._bounds.height;
             } else if (this.parent && this.parent.getBounds()) {
                 width = this.parent.getBounds().width;
                 height = this.parent.getBounds().height
+            } else if (this.getBounds()) {
+                width = this.getBounds().width;
+                height = this.getBounds().height;
             } else {
                 width = 0;
                 height = 0;
