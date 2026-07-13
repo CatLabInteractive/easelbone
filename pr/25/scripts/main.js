4b5242c897642cc0b3758a411920c9389418666f
daedeloth <daedeloth@users.noreply.github.com>
2026-07-13 14:38:00 +0000

    deploy: 27ad2b15e2f2bfdcde990c90915a673179a10fb6

---
+++ pr/25/scripts/main.js
@@ -0,0 +1,38 @@
+require.config ({
+	paths: {
+		easeljs: "vendor/EaselJS/lib/easeljs",
+		preloadjs: "vendor/PreloadJS/lib/preloadjs",
+		soundjs: "vendor/SoundJS/lib/soundjs",
+		tweenjs: "vendor/TweenJS/lib/tweenjs",
+		jquery: "vendor/jquery/dist/jquery",
+		requirejs: "vendor/requirejs/require",
+		underscore: "vendor/underscore/underscore",
+		backbone: "vendor/backbone/backbone",
+		easelhacks: "vendor/easelhacks/dist/scripts/easelhacks",
+		'emoji-regex' : "vendor/emoji-regex/index"
+	},
+	shim: {
+		easeljs: {
+			deps: [
+				"tweenjs"
+			],
+			exports: "createjs"
+		},
+		preloadjs: {
+			deps: [
+				"easeljs"
+			],
+			exports: "createjs"
+		}
+	},
+	packages: [
+
+	]
+});
+
+define (
+	[ 'easelbone' ],
+	function (catlabremote) {
+		return catlabremote;
+	}
+);
